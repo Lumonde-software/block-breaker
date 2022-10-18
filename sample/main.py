ba2ce9d3f2f59961666fc787ed0f487399269adc
Lumonde-software <nakamura-shoya-1@g.ecc.u-tokyo.ac.jp>
2022-10-18 13:19:55 +0900

    add directory

---
+++ sample/main.py
@@ -0,0 +1,15 @@
+import cv2
+
+face_cascade_path = '/home/denjo/experiment/cvgl/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
+
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
+
+src = cv2.imread('../data/lena.jpg')
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(src_gray)
+
+for x, y, w, h in faces:
+    cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    
+cv2.imwrite('../processed/lena_after.png', src)
