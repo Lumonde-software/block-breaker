146b66cb1b37840371bd8555e58a2fa33dc5fd74
Lumonde-software <nakamura-shoya-1@g.ecc.u-tokyo.ac.jp>
2022-10-18 16:32:18 +0900

    update block-breaker

--- sample/main.py
+++ sample/main.py
@@ -1,15 +1,16 @@
 import cv2
 
-face_cascade_path = '/home/denjo/experiment/cvgl/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
+def sample():
+    face_cascade_path = '/home/denjo/experiment/cvgl/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
 
-face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
-src = cv2.imread('../data/lena.jpg')
-src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    src = cv2.imread('data/man.webp')
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(src_gray)
+    faces = face_cascade.detectMultiScale(src_gray)
 
-for x, y, w, h in faces:
-    cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    
-cv2.imwrite('../processed/lena_after.png', src)
+    for x, y, w, h in faces:
+        cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        
+    cv2.imwrite('processed/man_after.png', src)
